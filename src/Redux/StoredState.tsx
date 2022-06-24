c2e461f924663335f5dd995b613e47c24d3e9d38
Mihail Schutow <schutow.mihail@yandex.ru>
2022-06-24 19:50:30 +0300

    HW done (?)

--- src/Redux/StoredState.tsx
+++ src/Redux/StoredState.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import {AppPropsType} from '../App';
+import {v1} from 'uuid';
+import {state2, updateNewPostText} from './State2';
 
 export type StoreType = {
-
+    rerenderEntireTree: (state2: AppPropsType) => void
+    state2: AppPropsType
+    addPost:()=> void
+    updateNewPostText: (newText: string) => void
+    subscribe: (observer: (state2: AppPropsType) => void) => void
 }
 
-export let store = {
+export let store: StoreType = {
+    rerenderEntireTree(state2: AppPropsType) {
+        console.log('State was changed')
+    },
+
+    state2: {
+        profilePage: {
+            myPostsData: [
+                {id: v1(), message: 'Hi, how are you?', likes: 22},
+                {id: v1(), message: 'Very nice!', likes: 8},
+                {id: v1(), message: 'It is my second post', likes: 19},
+                {id: v1(), message: 'It is my first post', likes: 15}
+            ],
+            newPostText: '',
+        },
+        dialogsPage: {
+            dialogsData: [
+                {id: v1(), name: 'Vasya', img: 'https://live.staticflickr.com/7572/26312703593_c983190d6c_b.jpg'},
+                {id: v1(), name: 'Kate', img: 'https://live.staticflickr.com/7572/26312703593_c983190d6c_b.jpg'},
+                {id: v1(), name: 'Leonid', img: 'https://live.staticflickr.com/7572/26312703593_c983190d6c_b.jpg'},
+                {id: v1(), name: 'Alex', img: 'https://live.staticflickr.com/7572/26312703593_c983190d6c_b.jpg'}
+            ],
+            messagesData: [
+                {
+                    id: v1(),
+                    message: 'Hi',
+                    img: 'https://i.pinimg.com/736x/3c/92/34/3c9234554d0f72bc4b131ae7ee830d20--bunny-bunny-bunny-rabbits.jpg'
+                },
+                {
+                    id: v1(),
+                    message: 'How are you?',
+                    img: 'https://i.pinimg.com/736x/3c/92/34/3c9234554d0f72bc4b131ae7ee830d20--bunny-bunny-bunny-rabbits.jpg'
+                },
+                {
+                    id: v1(),
+                    message: 'Very good',
+                    img: 'https://i.pinimg.com/736x/3c/92/34/3c9234554d0f72bc4b131ae7ee830d20--bunny-bunny-bunny-rabbits.jpg'
+                },
+            ],
+        }
+    },
+    addPost() {
+        /*let newPostState = */
+        state2.profilePage.myPostsData.push({id: v1(), message: state2.profilePage.newPostText, likes: 0})
+        this.rerenderEntireTree(state2);
+        updateNewPostText('');
+    },
+    updateNewPostText(newText: string){
+        state2.profilePage.newPostText = newText
+        this.rerenderEntireTree(state2)
+    },
+    subscribe(observer: (state2: AppPropsType) => void) {
+        this.rerenderEntireTree = observer
+    }
 
 }
