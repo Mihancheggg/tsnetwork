88de90799c75c87f1bf20265f07032b645f7bc7f
Mihail Schutow <schutow.mihail@yandex.ru>
2022-09-13 19:35:46 +0300

    Reselect library added

--- src/Redux/Selectors/UsersSelectors.ts
+++ src/Redux/Selectors/UsersSelectors.ts
@@ -1,9 +1,18 @@
 import {RootStateType} from '../ReduxStore';
 import {UserType} from '../Reducers/UsersReducer';
+import { createSelector } from 'reselect';
 
 export const getUsers = (state: RootStateType): Array<UserType> => {
     return state.usersReducer.users
 }
+
+export const getUsersSelector = (state: RootStateType): Array<UserType> => {
+    return getUsers(state).filter(u=> true)
+}
+
+export const getUsersSuperSelector = createSelector(getUsers,(users: Array<UserType>)=> {
+    return  users.filter(u=> true)
+})//rerender doesn't called while getUsers return same value
 
 export const getPageSize = (state: RootStateType): number => {
     return state.usersReducer.pageSize
