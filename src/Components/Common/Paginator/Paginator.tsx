222162aa3ada5cee05daf264396f3da93f5c55b2
Mihail Schutow <schutow.mihail@yandex.ru>
2022-11-12 11:04:53 +0300

    update eslint settings

--- src/Components/Common/Paginator/Paginator.tsx
+++ src/Components/Common/Paginator/Paginator.tsx
@@ -1,33 +1,32 @@
-import React, {useState} from 'react';
-import {v1} from 'uuid';
-import styles from './Paginator.module.css';
+import React, { useState } from 'react'
+import { v1 } from 'uuid'
+import styles from './Paginator.module.css'
 
-type PaginatorPropsType = {
-    totalUsersCount: number,
-    pageSize: number,
-    currentPage: number,
-    portionSize: number,
-    onPageChanged: (pageNumber: number) => void
+export type PaginatorPropsType = {
+  totalUsersCount: number
+  pageSize: number
+  currentPage: number
+  portionSize: number
+  onPageChanged: (pageNumber: number) => void
 }
 
-export const Paginator = ({totalUsersCount, pageSize, currentPage, portionSize, onPageChanged}: PaginatorPropsType) => {
+export const Paginator = ({ totalUsersCount, pageSize, currentPage, portionSize, onPageChanged }: PaginatorPropsType) => {
+  const pagesCount = Math.ceil(totalUsersCount / pageSize)
+  const portionsCount = Math.ceil(pagesCount / portionSize)
+  const [portionNumber, setPortionNumber] = useState<number>(1)
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+  const rightPortionPageNumber = portionNumber * portionSize
 
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
-    let portionsCount = Math.ceil(pagesCount / portionSize);
-    let [portionNumber, setPortionNumber] = useState<number>(1);
-    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-    let rightPortionPageNumber = portionNumber * portionSize
+  const pages: number[] = []
 
-    let pages: Array<number> = []
-
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
-    return <div className={styles.paginator}>
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
+  return <div className={styles.paginator}>
         {portionNumber > 1 && <button onClick={() => setPortionNumber(portionNumber - 1)}>PREV</button>}
         {pages
-            .filter(el => el >= leftPortionPageNumber && el <= rightPortionPageNumber)
-            .map(el => <span key={v1()}
+          .filter(el => el >= leftPortionPageNumber && el <= rightPortionPageNumber)
+          .map(el => <span key={v1()}
                              className={currentPage === el ? styles.selectedPage : ''}
                              onClick={(event) => onPageChanged(el)}>{el}</span>)}
         {portionNumber < portionsCount && <button onClick={() => setPortionNumber(portionNumber + 1)}>NEXT</button>}
