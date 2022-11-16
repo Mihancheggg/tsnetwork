a171c00b9ec2c2ef6c396215df929b87efc9c0fe
Mihail Schutow <schutow.mihail@yandex.ru>
2022-11-16 20:59:29 +0300

    feat form fields validators type

--- src/Utils/Validators/validators.ts
+++ src/Utils/Validators/validators.ts
@@ -1,18 +1,20 @@
-export const requiredField = (value: string) => {
+export type FieldValidatorType = (value: string) => string | undefined
+
+export const requiredField: FieldValidatorType = (value: string) => {
     if (value) {
         return undefined
     }
     return 'Field is required.'
 }
 
-export const maxLengthCreator = (maxLength: number) => (value: string) => {
+export const maxLengthCreator = (maxLength: number): FieldValidatorType => (value: string) => {
     if (value && value.length > maxLength) {
         return `Max length of the field is ${maxLength} symbols.`
     }
     return undefined
 }
 
-export const maxLength140 = (value: string) => {
+export const maxLength140: FieldValidatorType = (value: string) => {
     if (value && value.length > 140) {
         return 'Max length of the field is 140 symbols.'
     }
